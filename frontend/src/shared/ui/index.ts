cd774a86517d913742fceae5338e6cf947c181e8
Руслан <rkorobcenko153@gmail.com>
2026-06-25 08:22:49 +0300

    feat: add frontend/src/shared/ui/index.ts

---
+++ frontend/src/shared/ui/index.ts
@@ -0,0 +1,7 @@
+export { Button }  from "./Button";
+export { Input }   from "./Input";
+export { Badge }   from "./Badge";
+export { Spinner } from "./Spinner";
+export { Modal }   from "./Modal";
+export { Toaster } from "./Toaster";
+export { cn }      from "./cn";
